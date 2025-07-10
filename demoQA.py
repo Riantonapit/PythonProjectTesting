e22ec4c77119417727ce716554ce7a3c49f92022
Riantonapit <ryanleonel8@gmail.com>
2025-07-10 18:24:55 +0700

    update demoQA.py after fixing chrome issue

--- demoQA.py
+++ demoQA.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
+
+options = Options()
+user_data_dir = tempfile.mkdtemp()
+options.add_argument(f' --user-data-dir={user_data_diri}')
 
 options = webdriver.ChromeOptions()
 
